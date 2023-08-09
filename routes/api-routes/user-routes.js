edb2e75bc416c3be0f81476dd9dc02b42a61862b
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2023-08-09 15:55:03 -0700

    user-routes initial set-up

---
+++ routes/api-routes/user-routes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+
+const {
+  getAllUsers,
+  getUsersById,
+  createUser,
+  updateUserById,
+  deleteUser,
+  addFriend,
+  deleteFriend
+} = require('../../controllers/users');
+
+router.route('/').get(getAllUsers).post(createUser);
+
+router.route('/:userId').get(getUsersById).put(updateUserById).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+
+module.exports = router;
